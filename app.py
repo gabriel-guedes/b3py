0495c28732ecf28e7ac1db008e51b9fe3f4d1341
gabriel <gabriel.guedes@gmail.com>
2020-12-03 17:07:18 -0300

    lazy read function

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
-from b3py.history import read_file
+from b3py.history import read_file, lazy_read
 #COTAHIST_D10112020.TXT
 sample_file = './b3py/sample/COTAHIST_D10112020.TXT'
 
-values = read_file(sample_file)
-for i in values:
-    print(i)
+# values = read_file(sample_file)
+# for i in values:
+#     print(i)
 
+# def firstn(n):
+#     num = 0
+#     while num < n:
+#         yield num
+#         num += 1
+
+# sum_of_first_n = sum(firstn(6))
+# print(sum_of_first_n)
+
+# first_five = firstn(4)
+
+# print(next(first_five))
+# print(next(first_five))
+# print(next(first_five))
+# print(next(first_five))
+
+values = lazy_read(sample_file)
+line_count = 0
+for i in values:
+    line_count = line_count + len(i)
+
+# while True:
+#     try:
+#         line_count = line_count + (len(next(values)))
+#     except StopIteration:
+#         print('Cheguei no fim')
+#         break
+
+print(f'LAZY lines: {line_count}')
+
+values = read_file(sample_file)
+# print(values[:10])
+print(f'Full read lines: {len(values)}')
--- b3py/history.py
+++ b3py/history.py
@@ -28,3 +28,40 @@ def read_file(filepath:str)->list:
             values.append(line_values)
     
     return values
+
+def lazy_read(filepath:str, step=10)->list:
+    values = []
+    i = 0
+    with open(filepath, 'r') as rawdata:
+        for line in rawdata:
+            if i == 0:
+                values = []
+            parsed = get_values(line)
+            if parsed['rec_type'] in ['00', '99']:
+                continue
+            if parsed['market_type'] != '010':
+                continue
+            if parsed['bdi_code'] != '02':
+                continue
+
+            line_values = [
+                parsed['session_date'],
+                parsed['ticker'],
+                parsed['open_price'],
+                parsed['high_price'],
+                parsed['low_price'],
+                parsed['close_price'],
+                parsed['volume'],
+                parsed['quantity'],
+                parsed['deals']
+            ]
+            
+            values.append(line_values)
+            # yield line_values
+            i += 1
+
+            if i == step:
+                i = 0
+                yield values
+        
+        yield values
